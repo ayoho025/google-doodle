4898af8b59f4316391984b799f07672c37bb9b3a
ayoho025 <74688417+ayoho025@users.noreply.github.com>
2021-03-18 17:27:01 -0700

    Layering, Posistioning, and some animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-console.log('OK');
+// var loop = gsap.timeline({ repeat: -1, repeatDelay: 2.5 });
+// loop.to("#clouds", { x: -450, duration: 5 });
+
+var cloudOne = TweenMax.fromTo(
+  ".clouds-1-1",
+  20,
+  { x: 434 },
+  { x: -434, ease: Linear.easeNone, repeat: -1 }
+);
+var cloudOneTwo = TweenMax.fromTo(
+  ".clouds-1-2",
+  20,
+  { x: 434 },
+  { x: -434, ease: Linear.easeNone, repeat: -1, delay: 10 }
+);
+cloudOne.play();
+cloudOneTwo.play();
