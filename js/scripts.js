88b2636cf29c96b56e518c9587c199e2c2a7da1f
ayoho025 <74688417+ayoho025@users.noreply.github.com>
2021-03-18 22:10:22 -0700

    worked on clouds

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,22 @@
 
 var cloudOne = TweenMax.fromTo(
   ".clouds-1-1",
-  20,
-  { x: 434 },
-  { x: -434, ease: Linear.easeNone, repeat: -1 }
+  10,
+  { x: 0 },
+  { x: -434, ease: Linear.easeNone }
 );
 var cloudOneTwo = TweenMax.fromTo(
   ".clouds-1-2",
   20,
   { x: 434 },
-  { x: -434, ease: Linear.easeNone, repeat: -1, delay: 10 }
+  { x: -434, ease: Linear.easeNone, repeat: -1, repeatDelay: -10 }
+);
+var cloudOneThree = TweenMax.fromTo(
+  ".clouds-1-3",
+  20,
+  { x: 434 },
+  { x: -434, ease: Linear.easeNone, repeat: -1, delay: 10, repeatDelay: -10 }
 );
 cloudOne.play();
 cloudOneTwo.play();
+cloudOneThree.play();
