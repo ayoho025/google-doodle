cbff1c269161593fd4e258f0a0e2d7a19007201e
ayoho025 <74688417+ayoho025@users.noreply.github.com>
2021-03-18 22:35:22 -0700

    Clouds Are Done!

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,64 @@
 
 var cloudOne = TweenMax.fromTo(
   ".clouds-1-1",
-  10,
+  15,
   { x: 0 },
-  { x: -434, ease: Linear.easeNone }
+  { x: -434, ease: Linear.easeNone, repeat: -1 }
 );
 var cloudOneTwo = TweenMax.fromTo(
   ".clouds-1-2",
-  20,
+  15,
   { x: 434 },
-  { x: -434, ease: Linear.easeNone, repeat: -1, repeatDelay: -10 }
+  { x: 0, ease: Linear.easeNone, repeat: -1 }
 );
-var cloudOneThree = TweenMax.fromTo(
-  ".clouds-1-3",
-  20,
-  { x: 434 },
-  { x: -434, ease: Linear.easeNone, repeat: -1, delay: 10, repeatDelay: -10 }
-);
+
 cloudOne.play();
 cloudOneTwo.play();
-cloudOneThree.play();
+
+var cloudTwo = TweenMax.fromTo(
+  ".clouds-2-1",
+  20,
+  { x: 0 },
+  { x: -434, ease: Linear.easeNone, repeat: -1 }
+);
+var cloudTwoTwo = TweenMax.fromTo(
+  ".clouds-2-2",
+  20,
+  { x: 434 },
+  { x: 0, ease: Linear.easeNone, repeat: -1 }
+);
+
+cloudTwo.play();
+cloudTwoTwo.play();
+
+var cloudThree = TweenMax.fromTo(
+  ".clouds-3-1",
+  25,
+  { x: 0 },
+  { x: -434, ease: Linear.easeNone, repeat: -1 }
+);
+var cloudThreeTwo = TweenMax.fromTo(
+  ".clouds-3-2",
+  25,
+  { x: 434 },
+  { x: 0, ease: Linear.easeNone, repeat: -1 }
+);
+
+cloudThree.play();
+cloudThreeTwo.play();
+
+var cloudFour = TweenMax.fromTo(
+  ".clouds-4-1",
+  30,
+  { x: 0 },
+  { x: -434, ease: Linear.easeNone, repeat: -1 }
+);
+var cloudFourTwo = TweenMax.fromTo(
+  ".clouds-4-2",
+  30,
+  { x: 434 },
+  { x: 0, ease: Linear.easeNone, repeat: -1 }
+);
+
+cloudFour.play();
+cloudFourTwo.play();
